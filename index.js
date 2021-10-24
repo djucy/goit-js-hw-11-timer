298fc4fd835f65dcb573b1a2d01a920b938da3e3
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2021-10-25 00:39:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-
-
-
-
-
 class CountdownTimer {
     constructor({ selector, targetDate}) {
         this.selector = selector;
         this.targetDate = targetDate;
      }
-    getRefs = () => {
+    getRefs () {
         const container = document.querySelector(this.selector);
+        const days = container.querySelector('[data-value="days"]');
+        const hours = container.querySelector('[data-value="hours"]');
+        const mins = container.querySelector('[data-value="mins"]');
+        const secs = container.querySelector('[data-value="secs"]');
+        return { days, hours, mins, secs };
+    }
+    startTimer ({ days, hours, mins, secs }) {
+        const time= this.targetDate - Date.now();
+        days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        hours.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        mins.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        
+    }
+    pad(value) {
+        return String(value).padStart(2, "0");
+    }
+    start () {
+        setInterval(() => {
+            this.startTimer(this.getRefs())
+        })
      }
-
- }
-
-
-
-
-
-// new CountdownTimer({
-//   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
-// });
+}
+ 
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2019'),
+});
+timer.start();
